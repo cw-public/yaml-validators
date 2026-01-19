54c17366706918e863e102afa3e97972bc8912a9
artur <artur.hryhorychenko@controlware.de>
2026-01-19 05:16:34 +0100

    adjusted setup by adding py_modules

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='yaml-validators',                    # Package-Name
-    install_requires=['ruamel.yaml>=0.17.0'],  # Dependencies automatisch installieren
+    name='yaml-validators',
+    version='1.0.0',
+    py_modules=[
+        'yaml_router',
+        'kubernetes_validator',
+        'helm_validator',
+    ],
+    install_requires=['ruamel.yaml>=0.17.0'],
 )
