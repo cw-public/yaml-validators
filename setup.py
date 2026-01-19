49f70610da10c0bc097fd8900740054aafc8f482
artur <artur.hryhorychenko@controlware.de>
2026-01-19 05:11:16 +0100

    add setup for hook

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='yaml-validators',                    # Package-Name
+    install_requires=['ruamel.yaml>=0.17.0'],  # Dependencies automatisch installieren
+)
